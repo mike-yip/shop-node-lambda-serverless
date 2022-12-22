c2c1dec3ceaf0a78caab722d8b8d368f2d934a76
Mike Yip <mike.yip@hkjc.org.hk>
2022-12-22 10:56:44 +0800

    APIs quick start

---
+++ api/productService.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// mock product data
+const products = [
+  {
+    description: "Short Product Description1",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+    price: 24,
+    title: "ProductOne",
+  },
+  {
+    description: "Short Product Description7",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+    price: 15,
+    title: "ProductTitle",
+  },
+  {
+    description: "Short Product Description2",
+    id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+    price: 23,
+    title: "Product",
+  },
+  {
+    description: "Short Product Description4",
+    id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+    price: 15,
+    title: "ProductTest",
+  },
+  {
+    description: "Short Product Descriptio1",
+    id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+    price: 23,
+    title: "Product2",
+  },
+  {
+    description: "Short Product Description7",
+    id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+    price: 15,
+    title: "ProductName",
+  },
+];
+
+module.exports.getProductsList = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(products),
+  };
+};
+
+module.exports.getProductsById = async (event) => {
+  const productId = event.pathParameters.productId || null;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      products.find((product) => product.id === productId) || null
+    ),
+  };
+};
