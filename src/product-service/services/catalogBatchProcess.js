60dbb5e9e001df58f66a5824d23beaabba08e804
Mike Yip <mike_yip@epam.com>
2023-01-04 14:06:50 +0800

    add createProductTopic SNS topic

--- src/product-service/services/catalogBatchProcess.js
+++ src/product-service/services/catalogBatchProcess.js
@@ -1,17 +1,58 @@
 "use strict";
 import { createProduct } from "../models/product";
+import AWS from "aws-sdk";
+
+const getCreateProductTopicArn = () => process.env["CREATE_PRODUCT_TOPIC_ARN"];
+
+const getSnsInstance = (() => {
+  let sns = null;
+  return () => {
+    if (!sns) sns = new AWS.SNS();
+    return sns;
+  };
+})();
+
+const sendSnsMessage = async (productsCreated, productsFailed) => {
+  const message = `Created: ${JSON.stringify(productsCreated)}\n\nFailed: ${JSON.stringify(productsFailed)}`;
+
+  var params = {
+    Message: message,
+    TopicArn: getCreateProductTopicArn(),
+  };
+
+  const sns = getSnsInstance();
+
+  await sns.publish(params).promise();
+};
 
 export const catalogBatchProcessApi = async (event) => {
-  for (const { body } of event?.Records || []) {
-    const product = JSON.parse(body);
-    const response = await createProduct({
-      ...product,
-      price: parseInt(product.price, 10),
-      count: parseInt(product.count, 10),
-    });
+  const productsCreated = []; // [product in JSON, ...]
+  const productsFailed = []; // [{ product in json, message }, ...]
 
-    if (!response.created) {
-      throw new Error(response.message);
+  const addFailedProduct = (productJson, message) => {
+    productsFailed.push({ json: productJson, message });
+  };
+
+  for (const { body } of event?.Records || []) {
+    try {
+      const product = JSON.parse(body);
+      const productToAdd = {
+        ...product,
+        price: parseInt(product.price, 10),
+        count: parseInt(product.count, 10),
+      };
+
+      const response = await createProduct(productToAdd);
+
+      if (response.created) {
+        productsCreated.push(JSON.stringify(productToAdd));
+      } else {
+        addFailedProduct(body, response.message);
+      }
+    } catch (e) {
+      addFailedProduct(body, e.message);
     }
   }
+
+  await sendSnsMessage(productsCreated, productsFailed);
 };
